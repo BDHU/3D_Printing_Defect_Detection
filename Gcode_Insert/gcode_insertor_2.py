e69f0b909c5b6e3ecb011bd01656a37dc14087e4
BDHU <bodunhu@utexas.edu>
2017-06-22 15:26:17 -0500

    gcode insertor V1.0 finished

--- Gcode_Insert/gcode_insertor_2.py
+++ Gcode_Insert/gcode_insertor_2.py
@@ -1,10 +1,12 @@
 import re
+import fileinput
+import os
 
 # check to see of the line matches pattern G# X# Y# Z# F# A#
 # ignore everything after ; if the line has it
 # TODO check error
 def match_patterns(line):
-	pattern = re.compile("^(G[0-9]+) X([-+]?\d*\.\d+|\d+) Y([-+]?\d*\.\d+|\d+) Z([-+]?\d*\.\d+|\d+) F([-+]?\d*\.\d+|\d+) A([-+]?\d*\.\d+|\d+).*$")
+	pattern = re.compile("^(G[0-9]+) X([-+]?\d*\.\d+|\d+) Y([-+]?\d*\.\d+|\d+) Z([-+]?\d*\.\d+|\d+) F([-+]?\d*\.\d+|\d+) A([-+]?\d*\.\d+|\d+);.*$")
 	return pattern.match(line)
 
 def get_Z_val(line):
@@ -30,32 +32,26 @@ original_Z = 0
 is_first_time_match = True
 layers_to_insert = 3
 layers_till_insert = 0
-instructions_to_insert = "\nM28\n"
+instructions_to_insert = "G28\n"
 
-# with file_object as f:
-# 	contents = f.readlines()
-with open(gcode_file_name, "r+") as f:
-	contents = f.readlines()
-	for i, line in enumerate(contents):
-		# get the starting Z value
-		if is_first_time_match:
-			original_Z = get_init_Z(line)
-			Z_val = original_Z
-			if original_Z != -1:
-				is_first_time_match = False
-			continue
-			# evaluate if the line contains a "Z" char
-		# if has_Z_val(line):
-		# 	# process to the next step to see if it matches the pattern
-		if match_patterns(line):
-			# if match the pattern, get the Z value
-			Z_val = get_Z_val(line)
-			dif = Z_val - original_Z
-			if dif != 0:
-				layers_till_insert = layers_till_insert+1
-				if layers_till_insert == layers_to_insert:
-					layers_till_insert = 0
-					# write to specific location
-					f.write(instructions_to_insert)
-# fd.close()
-f.close()
+
+for line in fileinput.FileInput("/home/ed/Desktop/3D_Printing_Defect_Detection/Gcode_Insert/left_half_test_maker.gcode", inplace=1):
+	if is_first_time_match:
+		original_Z = get_init_Z(line)
+		Z_val = original_Z
+		if original_Z != -1:
+			is_first_time_match = False
+		print(line, end='')
+		continue
+	if match_patterns(line):
+		Z_val = get_Z_val(line)
+		dif = Z_val - original_Z
+		if dif != 0:
+			print('dif: ', dif)
+			original_Z = Z_val
+			layers_till_insert = layers_till_insert + 1
+			if layers_till_insert == layers_to_insert:
+				layers_till_insert = 0
+				line = line.replace(line, instructions_to_insert+line)
+	print(line, end='')
+
--- Gcode_Insert/test.py
+++ Gcode_Insert/test.py
@@ -1,16 +1,19 @@
+import fileinput
 import re
 
 def match_patterns(line):
 	pattern = re.compile("^(G[0-9]+) X([-+]?\d*\.\d+|\d+) Y([-+]?\d*\.\d+|\d+) Z([-+]?\d*\.\d+|\d+) F([-+]?\d*\.\d+|\d+) A([-+]?\d*\.\d+|\d+).*$")
 	return pattern.match(line)
+#file_postfix = ".gcode"
+#usr_input = input("Please specify the name of the file without the postfix: ")
+#gcode_file_name = usr_input + file_postfix
 
-file_postfix = ".gcode"
-usr_input = input("Please specify the name of the file without the postfix: ")
-gcode_file_name = usr_input + file_postfix
+for line in fileinput.FileInput("/home/ed/Desktop/3D_Printing_Defect_Detection/Gcode_Insert/left_half_test_maker.gcode", inplace=1):
+	if match_patterns(line):
+		line = line.replace(line, line + " gotta")
+	print(line,)
 
-with open(gcode_file_name, "r+") as f:
-	contents = f.readlines()
-	for i, line in enumerate(contents):
-		if match_patterns(line):
-			print(line)
-			print("fuck ", i)
+
+
+
+
